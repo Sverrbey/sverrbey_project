67c88da43c73f398edbbf4567c785fb8be2fb563
SverreB <sverrbey@stud.ntnu.no>
2025-03-27 16:38:48 +0100

    removing .venv stuff

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/SverreB/Github_Repo/sverrbey_project_fresh/.venv"
+set -gx VIRTUAL_ENV "/Users/SverreB/Github_Repo/sverrbey_project/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
